f1458335cfa737b1817b3d412e7161ae1f6ef77f
ruben-nithyaganesh <rubennithyaganesh@gmail.com>
2024-04-25 23:28:22 +1200

    extra safe drawing rectangle

--- audiowave.cpp
+++ audiowave.cpp
@@ -5,6 +5,12 @@
 #include <windows.h>
 #include <stdio.h>
 
+struct rect_state 
+{
+	int x;
+	int y;
+};
+
 struct window_buffer
 {
   BITMAPINFO bitmap_info;
@@ -35,6 +41,7 @@ static boolean running;
 RECT window_rect;
 static struct window_buffer window_buffer;
 static struct window_dimension window_dim;
+static struct rect_state rect_state;
 
 
 void set_colour(unsigned char red, unsigned char green, unsigned char blue)
@@ -74,8 +81,8 @@ void window_resized(HWND hwnd)
 	int w_width = window_rect.right - window_rect.left;
 	int w_height = window_rect.bottom - window_rect.top;
 	char output_msg[100];
-	sprintf(output_msg, "width: %d\nheight: %d\n", w_width, w_height);
-	OutputDebugStringA(output_msg);
+//	sprintf(output_msg, "width: %d\nheight: %d\n", w_width, w_height);
+//	OutputDebugStringA(output_msg);
 
 	window_dim.top = window_rect.top;
 	window_dim.left = window_rect.left;
@@ -132,7 +139,15 @@ void render_rect(struct window_buffer *b, int top, int left, int width, int heig
 	int b_width = b->width;
 	int b_height = b->height;
 	int pitch = bytes_per_pixel * b_width;
-
+	
+	if(top < 0)
+	{
+		top = 0;
+	}
+	if(left < 0)
+	{
+		left = 0;
+	}
 	if((left+width) > b_width)
 	{
 		width = b_width - left;
@@ -159,31 +174,102 @@ void render_rect(struct window_buffer *b, int top, int left, int width, int heig
 
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
+
+	LRESULT result = 0;
+
 	switch (uMsg)
 	{
-		case WM_DESTROY:
+		case WM_DESTROY: case WM_QUIT: case WM_CLOSE:
 		{
 				PostQuitMessage(0);
+				running = false;
 				return 0;
 		}
 		case WM_PAINT:
 		{
-			PAINTSTRUCT ps;
-			HDC hdc = BeginPaint(hwnd, &ps);
-			window_resized(hwnd);
-			render_colour(&window_buffer);
-			set_colour(0, 0, 0);
-			render_rect(&window_buffer, 100, 100, 300, 100);
-			display_buffer(hdc, &window_buffer);
-			EndPaint(hwnd, &ps);
+//			PAINTSTRUCT ps;
+//			HDC hdc = BeginPaint(hwnd, &ps);
+//			window_resized(hwnd);
+//			render_colour(&window_buffer);
+//			set_colour(0, 0, 0);
+//			render_rect(&window_buffer, 100, 100, 300, 100);
+//			display_buffer(hdc, &window_buffer);
+//			EndPaint(hwnd, &ps);
 		}
 		case WM_SIZE:
 		{
 			window_resized(hwnd);
 			resize_buffer(&window_buffer, window_dim.width, window_dim.height);
+			render_colour(&window_buffer);
+		}
+		case WM_SYSKEYDOWN:
+		case WM_KEYDOWN:
+		case WM_KEYUP:
+		case WM_SYSKEYUP:
+		{
+			int VKCode = wParam;
+			bool was_down = ((wParam & (1 << 30)) != 0);
+			bool is_down = ((wParam & (1 << 31)) == 0);
+			if(is_down && !was_down){
+				
+				if(VKCode == 'W')
+				{
+					rect_state.y -= 1;
+				}
+				else if(VKCode == 'A')
+				{
+					rect_state.x -= 1;
+				}
+				else if(VKCode == 'S')
+				{
+					rect_state.y += 1;
+				}
+				else if(VKCode == 'D')
+				{
+					rect_state.x += 1;
+				}
+				else if(VKCode == 'Q')
+				{
+					PostQuitMessage(0);
+					running = false;
+					return 0;
+				}
+				else if(VKCode == 'E')
+				{
+					
+				}
+				else if(VKCode == VK_UP)
+				{
+					
+				}
+				else if(VKCode == VK_LEFT)
+				{
+					
+				}
+				else if(VKCode == VK_DOWN)
+				{
+					
+				}
+				else if(VKCode == VK_RIGHT)
+				{
+					
+				}
+				else if(VKCode == VK_ESCAPE)
+				{
+					
+				}
+				else if(VKCode == VK_SPACE)
+				{
+					
+				}
+			}
+		}
+		default:
+		{
+			result = DefWindowProc(hwnd, uMsg, wParam, lParam);
 		}
 	}
-	return DefWindowProc(hwnd, uMsg, wParam, lParam);
+	return result;
 }
 
 int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, int nCmdShow)
@@ -224,19 +310,35 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine
 		return -1;
 	}
 
+	rect_state.x = 100;
+	rect_state.y = 100;
 	set_colour(200, 100, 200);
 	running = true;
 
 	ShowWindow(hwnd, nCmdShow);
 
+	HDC hdc = GetDC(hwnd);
 	window_resized(hwnd);
 	resize_buffer(&window_buffer, window_dim.width, window_dim.height);
 	//do the msg loop
 	MSG msg = {};
 	
-	while(GetMessage(&msg, NULL, 0, 0) > 0)
+	while(running)
 	{
-		TranslateMessage(&msg);
-		DispatchMessage(&msg);
+		MSG msg;
+		while(PeekMessage(&msg, hwnd, 0, 0, PM_REMOVE) > 0)
+		{
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+		
+		window_resized(hwnd);
+		set_colour(100, 50, 100);
+		render_colour(&window_buffer);
+		set_colour(50, 100, 50);
+		render_rect(&window_buffer, rect_state.y, rect_state.x, 300, 100);
+		display_buffer(hdc, &window_buffer);
 	}
+
+	return 0;
 }
